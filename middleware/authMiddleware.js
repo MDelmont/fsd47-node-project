eb6c96661d8e342fdfd39835ad8c2af3b55fb35e
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-07-05 09:41:31 +0200

    edit comments in middlewares

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,34 +1,33 @@
 import jwt from 'jsonwebtoken';
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 dotenv.config();
 const secretKey = process.env.SECRET_KEY_JWT_AUTH;
 /***
- * authMiddleware is a fonction midleware for check if user authenticate 
+ * authMiddleware is a middleware that checks if user is authenticated
  */
 const authMiddleware = (req, res, next) => {
-  console.log('start check auth')
-  // Get the token from the request headers
+  console.log('start check auth');
+  // Get the token from the session
   const token = req.session.token;
   // Check if there is a token
   if (!token) {
-    return res.redirect("auth/login");
+    return res.redirect('auth/login');
   }
 
   try {
     // Verify the token with jwt
-   
     const decoded = jwt.verify(token, secretKey);
 
     // add user id to the req auth
     req.auth = {
-        userId: decoded.userId
+      userId: decoded.userId,
     };
-  
+
     next();
   } catch (error) {
-    console.error('Error in authMiddleware')
-    console.error(error)
-    return res.redirect("auth/login");
+    console.error('Error in authMiddleware');
+    console.error(error);
+    return res.redirect('auth/login');
   }
 };
 
--- middleware/checkScopeUser.js
+++ middleware/checkScopeUser.js
@@ -1,20 +1,22 @@
 /***
- * checkScopeUser is a fonction midleware for check if userauth can update user 
+ * checkScopeUser is a middleware that checks if auth user can update user profile
  */
 const checkScopeUser = async (req, res, next) => {
-    try{
-    // Get the token from the request headers
-    const userId = req.auth.userId
-    const userModifyId = req.params.userId
-    // Check if there is a token
+  try {
+    // Get the id of the authentified user from req
+    const userId = req.auth.userId;
+    // Get the id from the params
+    const userModifyId = req.params.userId;
+    // Check if the 2 ids we got are equivalent or if the auth user is admin and if not, redirect
+    // because a user cannot update the profile of another user except if he is admin
     if (userId != userModifyId && !req.session.isAdmin) {
-        return res.redirect("/user/list");
+      return res.redirect('/user/list');
     }
 
-    next()
-    } catch (error) {
-        return res.redirect("/user/list");
-    }
+    next();
+  } catch (error) {
+    return res.redirect('/user/list');
+  }
 };
 
 export default checkScopeUser;
